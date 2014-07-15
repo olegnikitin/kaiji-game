79d28e652fd9febee7f299202bcddb015f0dab55
alf2918@gmail.com <alf2918@gmail.com@6c5019a7-b76b-d066-9d9c-4a69a07ec568>
2014-07-15 15:18:52 +0000

    Hashed user passwords using bcrypt. Password lendth is 60 characters.

--- trunk/database/insert.sql
+++ trunk/database/insert.sql
@@ -2,20 +2,20 @@ INSERT INTO `user` VALUES ('1', 'GUEST', 'UNKNOWN', '-', 'GUEST', 'GUEST', null)
 INSERT INTO `user` VALUES ('2', 'EASY_BOT', 'UNKNOWN', '-', 'EASY_BOT', 'EASY_BOT', null);
 INSERT INTO `user` VALUES ('3', 'MEDIUM_BOT', 'UNKNOWN', '-', 'MEDIUM_BOT', 'MEDIUM_BOT', null);
 INSERT INTO `user` VALUES ('4', 'HARD_BOT', 'UNKNOWN', '-', 'HARD_BOT', 'HARD_BOT', null);
-INSERT INTO `user` VALUES ('5', 'user1', 'MALE', '111', 'user1', 'user1', null);
-INSERT INTO `user` VALUES ('6', 'user2', 'MALE', '222', 'user2', 'user2', null);
-INSERT INTO `user` VALUES ('7', 'user3', 'MALE', '333', 'user3', 'user3', null);
-INSERT INTO `user` VALUES ('8', 'user4', 'MALE', '444', 'user4', 'user4', null);
-INSERT INTO `user` VALUES ('9', 'user5', 'MALE', '555', 'user5', 'user5', null);
-INSERT INTO `user` VALUES ('10', 'admin', 'MALE', 'admin', 'admin', 'admin', null);
+#INSERT INTO `user` VALUES ('5', 'user1', 'MALE', '111', 'user1', 'user1', null);
+#INSERT INTO `user` VALUES ('6', 'user2', 'MALE', '222', 'user2', 'user2', null);
+#INSERT INTO `user` VALUES ('7', 'user3', 'MALE', '333', 'user3', 'user3', null);
+#INSERT INTO `user` VALUES ('8', 'user4', 'MALE', '444', 'user4', 'user4', null);
+#INSERT INTO `user` VALUES ('9', 'user5', 'MALE', '555', 'user5', 'user5', null);
+#INSERT INTO `user` VALUES ('10', 'admin', 'MALE', 'admin', 'admin', 'admin', null);
 
 INSERT INTO `user_role` VALUES ('1', 'GUEST_ROLE');
 
-INSERT INTO `user_role` VALUES ('5', 'USER_ROLE');
-INSERT INTO `user_role` VALUES ('6', 'USER_ROLE');
-INSERT INTO `user_role` VALUES ('7', 'USER_ROLE');
-INSERT INTO `user_role` VALUES ('8', 'USER_ROLE');
-INSERT INTO `user_role` VALUES ('9', 'USER_ROLE');
+#INSERT INTO `user_role` VALUES ('5', 'USER_ROLE');
+#INSERT INTO `user_role` VALUES ('6', 'USER_ROLE');
+#INSERT INTO `user_role` VALUES ('7', 'USER_ROLE');
+#INSERT INTO `user_role` VALUES ('8', 'USER_ROLE');
+#INSERT INTO `user_role` VALUES ('9', 'USER_ROLE');
 
-INSERT INTO `user_role` VALUES ('10', 'USER_ROLE');
-INSERT INTO `user_role` VALUES ('10', 'ADMIN_ROLE');
+#INSERT INTO `user_role` VALUES ('10', 'USER_ROLE');
+#INSERT INTO `user_role` VALUES ('10', 'ADMIN_ROLE');
