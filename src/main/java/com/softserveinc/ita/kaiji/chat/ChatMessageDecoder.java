7306a076522b0280fbea8ef089c85a0187e4492f
alf2918 <alf2918@gmail.com>
2014-08-01 10:25:41 +0000

    Added websocket chat logic.

---
+++ src/main/java/com/softserveinc/ita/kaiji/chat/ChatMessageDecoder.java
@@ -0,0 +1,41 @@
+package com.softserveinc.ita.kaiji.chat;
+
+import java.io.StringReader;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.websocket.DecodeException;
+import javax.websocket.Decoder;
+import javax.websocket.EndpointConfig;
+
+public class ChatMessageDecoder implements Decoder.Text<ChatMessage> {
+	
+    private final Logger log = Logger.getLogger(getClass().getName());
+    
+    @Override
+	public void init(final EndpointConfig config) {
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public ChatMessage decode(final String textMessage) throws DecodeException {
+	    log.info("Decode message " + textMessage);
+	    ChatMessage chatMessage = new ChatMessage();
+		JsonObject obj = Json.createReader(new StringReader(textMessage))
+				.readObject();
+		chatMessage.setMessage(obj.getString("message"));
+		chatMessage.setSender(obj.getString("sender"));
+		chatMessage.setReceived(new Date());
+		return chatMessage;
+	}
+
+	@Override
+	public boolean willDecode(final String s) {
+		return true;
+	}
+}
---
+++ src/main/java/com/softserveinc/ita/kaiji/chat/ChatMessageEncoder.java
@@ -0,0 +1,32 @@
+package com.softserveinc.ita.kaiji.chat;
+
+import java.util.logging.Logger;
+
+import javax.json.Json;
+import javax.websocket.EncodeException;
+import javax.websocket.Encoder;
+import javax.websocket.EndpointConfig;
+
+public class ChatMessageEncoder implements Encoder.Text<ChatMessage> {
+	
+    private final Logger log = Logger.getLogger(getClass().getName());
+    
+    @Override
+	public void init(final EndpointConfig config) {
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public String encode(final ChatMessage chatMessage) throws EncodeException {
+	    log.info("Encode message " + chatMessage);
+	    
+	    return Json.createObjectBuilder()
+				.add("message", chatMessage.getMessage())
+				.add("sender", chatMessage.getSender())
+				.add("received", chatMessage.getReceived().toString()).build()
+				.toString();
+	}
+}
