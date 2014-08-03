c0f1ad3d2f57c1fd998a3cc010acc0379d234379
alf2918 <alf2918@gmail.com>
2014-08-03 19:17:47 +0000

    Fixed small bug with WebSocket client addresses.

--- src/main/java/com/softserveinc/ita/kaiji/model/util/CheckOnlineUsers.java
+++ src/main/java/com/softserveinc/ita/kaiji/model/util/CheckOnlineUsers.java
@@ -2,6 +2,7 @@ package com.softserveinc.ita.kaiji.model.util;
 
 import com.softserveinc.ita.kaiji.chat.ActiveUsers;
 import com.softserveinc.ita.kaiji.chat.ChatClientUpdateEndpoint;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSessionEvent;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpSessionListener;
 import javax.websocket.ContainerProvider;
 import javax.websocket.WebSocketContainer;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.URI;
 import java.util.Collections;
 import java.util.HashSet;
@@ -16,7 +18,8 @@ import java.util.Set;
 
 public class CheckOnlineUsers implements HttpSessionListener {
 
-    public static final String KEY = CheckOnlineUsers.class.getName();
+    private static final Logger LOG =  Logger.getLogger(CheckOnlineUsers.class);
+    private static final String KEY = CheckOnlineUsers.class.getName();
 
     private Set<String> activeUsers = Collections.synchronizedSet(new HashSet<String>());
 
@@ -29,12 +32,14 @@ public class CheckOnlineUsers implements HttpSessionListener {
         Set<String> users = (Set<String>) se.getSession().getServletContext().getAttribute("nicknames");
         if (users.remove(se.getSession().getAttribute("nickname"))) {
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-            String uri = "ws://localhost:8080/users";
+
             try {
+                InetAddress ip = InetAddress.getLocalHost();
+                String uri = "ws://" + ip.getHostAddress() + ":8080" +"/users" ;
                 container.connectToServer(ChatClientUpdateEndpoint.class,
                         URI.create(uri));
             } catch (IOException | javax.websocket.DeploymentException e) {
-                System.out.println(e.getMessage() + " " + URI.create(uri));
+               LOG.error("Failed to open client WebSocket. " + e.getMessage());
             }
         }
         ActiveUsers.setUsers(users);
