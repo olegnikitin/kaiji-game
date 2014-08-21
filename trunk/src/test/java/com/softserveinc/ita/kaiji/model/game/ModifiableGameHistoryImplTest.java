422d9bbde022af587372355e0aff6d327bbdce11
kyryllbardachov <kyryllbardachov@6c5019a7-b76b-d066-9d9c-4a69a07ec568>
2014-08-21 22:34:07 +0000

    add some tests

---
+++ trunk/src/test/java/com/softserveinc/ita/kaiji/model/game/ModifiableGameHistoryImplTest.java
@@ -0,0 +1,57 @@
+package com.softserveinc.ita.kaiji.model.game;
+
+import com.softserveinc.ita.kaiji.model.User;
+import com.softserveinc.ita.kaiji.model.player.HumanPlayer;
+import com.softserveinc.ita.kaiji.model.player.Player;
+import com.softserveinc.ita.kaiji.model.player.PlayerFactory;
+import com.softserveinc.ita.kaiji.model.player.bot.Bot;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.times;
+
+/**
+ * Created by kbardtc on 8/21/2014.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/test/resources/application-context-test.xml"})
+@WebAppConfiguration
+public class ModifiableGameHistoryImplTest {
+    @Autowired
+    PlayerFactory playerFactory;
+    @Mock
+    TwoPlayersWinnerStrategyImpl winnerStrategy;
+    @Mock
+    GameInfo gameInfo;
+    @InjectMocks
+    ModifiableGameHistoryImpl modifiableGameHistory;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void determineWinners() {
+        Bot testBot = (Bot) playerFactory.makePlayer(Bot.Types.EASY, 3);
+        Player player = new HumanPlayer(new User(), 3, 3);
+        Set<Player> players = new HashSet<>();
+        players.add(player);
+        players.add(testBot);
+        Mockito.when(gameInfo.getPlayers()).thenReturn(players);
+        modifiableGameHistory.determineWinners();
+        Mockito.verify(winnerStrategy,times(1)).getWinners(players);
+    }
+}
---
+++ trunk/src/test/java/com/softserveinc/ita/kaiji/model/game/TwoPlayerGameImplTest.java
@@ -0,0 +1,72 @@
+package com.softserveinc.ita.kaiji.model.game;
+
+import com.softserveinc.ita.kaiji.model.Card;
+import com.softserveinc.ita.kaiji.model.User;
+import com.softserveinc.ita.kaiji.model.player.HumanPlayer;
+import com.softserveinc.ita.kaiji.model.player.Player;
+import com.softserveinc.ita.kaiji.model.player.PlayerFactory;
+import com.softserveinc.ita.kaiji.model.player.bot.Bot;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+
+/**
+ * Created by Kyryll on 21.08.2014.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/test/resources/application-context-test.xml"})
+@WebAppConfiguration
+public class TwoPlayerGameImplTest {
+    @Autowired
+    PlayerFactory playerFactory;
+    @Mock
+    GameInfoImpl gameInfo;
+    @Mock
+    StateRoundImpl currentRound;
+    @Mock
+    GameFiniteStateMachine state;
+    @InjectMocks
+    TwoPlayerGameImpl twoPlayerGame;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void canContinueGame() throws Exception {
+        Bot testBot = (Bot) playerFactory.makePlayer(Bot.Types.EASY, 3);
+        Player player = new HumanPlayer(new User("a", "a", "a"), 3, 3);
+        Set<Player> players = new HashSet<>();
+        players.add(player);
+        players.add(testBot);
+        when(gameInfo.getPlayers()).thenReturn(players);
+        // PowerMock.expectNew(GameInfoImpl.class).andReturn(gameInfo);
+        twoPlayerGame.makeTurn(Card.PAPER, player);
+        Mockito.verify(gameInfo, atLeastOnce()).getPlayers();
+    }
+
+    @Test
+    public void interruptGame(){
+        twoPlayerGame.interruptGame();
+        Mockito.verify(gameInfo, times(1)).setGameFinishTime((Date) any());
+    }
+}
