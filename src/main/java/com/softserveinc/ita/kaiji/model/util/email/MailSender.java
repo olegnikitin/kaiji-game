8adb520f1c96243b27c47cf768d78dd96c775f30
SashkaSidorenko <SashkaSidorenko@gmail.com>
2014-08-26 18:48:30 +0000

    moved email realization to spring

--- src/main/java/com/softserveinc/ita/kaiji/model/util/email/MailSender.java
+++ src/main/java/com/softserveinc/ita/kaiji/model/util/email/MailSender.java
@@ -2,15 +2,10 @@ package com.softserveinc.ita.kaiji.model.util.email;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
-
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import javax.servlet.ServletContext;
-import java.util.Properties;
 
 @Component
 public class MailSender {
@@ -20,53 +15,31 @@ public class MailSender {
     @Autowired
     Environment env;
 
+    @Autowired
+    JavaMailSenderImpl javaMailSender;
+
     /**
      * Sends message with specified subject and text to recipient
      *
-     * @param recipientMail  email of recipient person
-     * @param messageSubject   email subject
-     * @param userNickName   user nickname
-     * @param userPassword   user password
-     * @return  true if message was successfully send, otherwise - false
+     * @param to           email of recipient person
+     * @param subject      email subject
+     * @param userNickname user nickname
+     * @param userPassword user password
+     * @return true if message was successfully send, otherwise - false
      */
-    public boolean send(String recipientMail, String messageSubject, String userNickName, String userPassword) {
+    public boolean send(String to, String subject, String userNickname, String userPassword) {
 
-        if ("".equals(recipientMail)) {
+        if ("".equals(to)) {
             LOG.error("Empty recipient email address ");
             return false;
         }
 
-        String from = env.getProperty("email.address");
-        final String username = env.getProperty("email.address");
-        final String password = env.getProperty("email.password");
-
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", env.getProperty("mail.smtp.host"));
-        props.put("mail.smtp.port", env.getProperty("mail.smtp.port"));
-
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
-
-        try {
-
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientMail));
-            message.setSubject(messageSubject);
-            message.setText(env.getProperty("message.body")+ "Nickname: "+userNickName+"\nPassword: "+userPassword);
-            Transport.send(message, message.getAllRecipients());
-            LOG.trace("Sent message successfully....");
-
-        } catch (MessagingException e) {
-            LOG.error("Can't sent message to recipient " + recipientMail + " " + e.getMessage());
-            return false;
-        }
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(env.getProperty("message.body") + "Nickname: " + userNickname + "\nPassword: " + userPassword);
+        javaMailSender.send(message);
+        LOG.trace("Sent message successfully....");
         return true;
     }
 
